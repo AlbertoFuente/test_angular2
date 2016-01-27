8ea4f285787b89a28cc4f9a8dbac8d75796779b9
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2016-01-27 20:03:05 +0100

    improved app/boot.ts

--- app/boot.ts
+++ app/boot.ts
@@ -1,5 +1,5 @@
 import {bootstrap}    from 'angular2/platform/browser'
-import {AppComponent} from './todo/todo.component'
+import {AppComponent} from './todo-component/todo-component'
 import {TabsComponent} from './tabs-component/tabs-component'
 
 bootstrap(AppComponent);
