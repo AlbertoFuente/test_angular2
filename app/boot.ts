2eddc7ffce2d04575e82f4b8b4029567d6f75707
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2016-01-27 19:52:48 +0100

    Improved app/boot.ts - bootstrap for tabsComponent

--- app/boot.ts
+++ app/boot.ts
@@ -1,4 +1,6 @@
 import {bootstrap}    from 'angular2/platform/browser'
 import {AppComponent} from './todo/todo.component'
+import {TabsComponent} from './tabs-component/tabs-component'
 
 bootstrap(AppComponent);
+bootstrap(TabsComponent);
